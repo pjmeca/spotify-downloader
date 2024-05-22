7f4b2d43ddc64635cc150b1d9aa832e2c77cf7bb
Pablo Meca <90964186+pjmeca@users.noreply.github.com>
2024-05-22 19:48:34 +0200

    feat: Display program start & end time

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "Starting program..."
+echo "Starting program at $(date +"%H:%M")..."
 
 /usr/local/bin/python /app/automated_run.py /app/tracking.yaml
 
-echo "Job finished."
+echo "Job finished at $(date +"%H:%M")."
 echo "Sleeping until next run."
